c3f85d80a875f091991af2b37114751b7fd26ca0
dinhthenhan <nhandtps40210@gmail.com>
2025-04-13 04:45:06 +0700

    update .env va index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const { MongoClient } = require('mongodb');
 
@@ -5,11 +6,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const uri = process.env.MONGO_URI;
-const dbName = "zeal"; 
+const dbName = "zeal";
 
 let db;
 
 async function connectToMongo() {
+  if (!uri) {
+    throw new Error("MONGO_URI is not defined in environment variables");
+  }
   const client = new MongoClient(uri);
   try {
     await client.connect();
@@ -24,10 +28,17 @@ connectToMongo();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send("Server is running!");
+});
+
 app.get('/api/products', async (req, res) => {
   try {
     const collection = db.collection('products');
     const data = await collection.find({}).toArray();
+    if (data.length === 0) {
+      return res.status(404).json({ message: "No products found" });
+    }
     res.json(data);
   } catch (err) {
     res.status(500).json({ error: err.message });
