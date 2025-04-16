331bdc77f70c6cc1757f242b6292f475d6009ba2
dinhthenhan <nhandtps40210@gmail.com>
2025-04-17 06:50:37 +0700

    Fix all

--- models/product.js
+++ models/product.js
@@ -4,11 +4,13 @@ const productSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   price: { type: Number, required: true, min: 0 },
   description: { type: String, trim: true },
-  image: { type: String, trim: true },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
-  stock_quantity: { type: Number, required: true, min: 0 },
-  subcategory: { type: mongoose.Schema.Types.ObjectId, ref: 'SubCategory' },
-  created_at: { type: Date, default: Date.now }
+  images: [{ type: String, trim: true }],
+  category: { type: mongoose.Schema, ref: 'Category', required: true },
+  stock: { type: Number, required: true, min: 0 },
+  ingredients: [{ type: String, trim: true }], 
+  usage_instructions: [{ type: String, trim: true }], 
+  special: [{ type: String, trim: true }], 
+  created_at: { type: Date, default: Date.now } 
 });
 
 module.exports = mongoose.model('Product', productSchema);
