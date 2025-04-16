df238a45f81adef512291653eeacddc1f97c9d30
dinhthenhan <nhandtps40210@gmail.com>
2025-04-17 00:58:37 +0700

    sua id products

--- routes/products.js
+++ routes/products.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const Product = require('../models/product');
 
 router.get('/', async (req, res) => {
