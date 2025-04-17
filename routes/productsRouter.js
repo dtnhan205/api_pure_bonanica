0b3ed083968feeb066aff75b758f696c434c4750
dtnhan205 <nhandtps40210@gmail.com>
2025-04-18 04:39:37 +0700

    fix upload products

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,7 +5,7 @@ const upload = require('../middlewares/upload');
 
 router.post('/', upload.array('images', 4), productController.createProduct);
 router.put('/:id', upload.array('images', 4), productController.updateProduct);
-
+router.get('/no-discount', productController.getProductsWithoutDiscount);
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
 router.delete('/:id', productController.deleteProduct);
