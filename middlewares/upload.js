82871421eac6cafae68bdff08e0349f4ec8651a5
dtnhan205 <nhandtps40210@gmail.com>
2025-04-18 04:48:31 +0700

    fix upload hinh

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/'); // Lưu ảnh vào thư mục uploads/
+    cb(null, path.join('public', 'images')); 
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + '-' + Math.round(Math.random() * 1E9);
