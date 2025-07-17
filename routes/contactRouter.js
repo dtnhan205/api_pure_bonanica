18aee3970e89631f1bee72aa7d8884954cbf1581
dinhthenhan <dinhthenhandako@gmail.com>
2025-07-17 20:45:39 +0700

    update router contact

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const contactController = require('../controllers/contactController');
 const jwt = require('jsonwebtoken');
-const {isAdmin } = require('../middlewares/auth');
+const {authMiddleware, isAdmin } = require('../middlewares/auth');
 
 
 
 router.post('/', contactController.createContact);
 
-router.get('/', isAdmin, contactController.getAllContacts);
-router.get('/:id', isAdmin, contactController.getContactById);
-router.put('/:id', isAdmin, contactController.updateContact);
-router.delete('/:id', isAdmin, contactController.deleteContact);
+router.get('/',authMiddleware, isAdmin, contactController.getAllContacts);
+router.get('/:id',authMiddleware, isAdmin, contactController.getContactById);
+router.put('/:id',authMiddleware, isAdmin, contactController.updateContact);
+router.delete('/:id',authMiddleware, isAdmin, contactController.deleteContact);
 
 
 module.exports = router;
