5be370fcc30dca5f8dc68f293b299452934edc75
dinhthenhan <dinhthenhandako@gmail.com>
2025-08-26 16:06:55 +0700

    update router chatbot

--- routes/ChatbotRouter.js
+++ routes/ChatbotRouter.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const chatController = require('../controllers/ChatBotController');
 const { authMiddleware, isAdmin } = require('../middlewares/auth'); 
 
-router.post('/session', authMiddleware, chatController.createOrGetSession);
-router.post('/send', authMiddleware, chatController.sendMessage);
-router.get('/history/:sessionId', authMiddleware, chatController.getChatHistory);
+router.post('/session', chatController.createOrGetSession);
+router.post('/send', chatController.sendMessage);
+router.get('/history/:sessionId', chatController.getChatHistory);
 router.delete('/session/:sessionId', authMiddleware, isAdmin, chatController.deleteSession); // Chỉ admin mới xóa
 
 module.exports = router;
